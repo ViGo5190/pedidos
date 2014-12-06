110764d0025261c962dfeb310bd8152a2890e0ca
Stan Gumeniuk <i@vigo.su>
2014-12-06 20:44:01 +0300

    [add] get and post data getters

--- public/index.php
+++ public/index.php
@@ -4,8 +4,15 @@
  */
 
 require_once(__DIR__ . '/../src/system/template.php');
+require_once(__DIR__ . '/../src/system/request.php');
+
+if (isset(requestGetGETData()['name'])) {
+    $name = requestGetGETData()['name'];
+} else {
+    $name = 'World';
+}
 
 echo compileTemplate(
     'test',
-    ['name' => 'World']
+    ['name' => $name]
 );
---
+++ src/system/request.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Stan Gumeniuk i@vigo.su
+ */
+
+/**
+ * @return mixed
+ */
+function requestGetPOSTData()
+{
+    return $_POST;
+}
+
+function requestGetGETData()
+{
+    return $_GET;
+}
+
