8fe71ea0fcbe55020327ea1566158b16cfb11a98
Stan Gumeniuk <i@vigo.su>
2014-12-09 13:40:04 +0300

    [add] loader src

--- public/index.php
+++ public/index.php
@@ -6,17 +6,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require_once(__DIR__ . '/../src/system/config.php');
-require_once(__DIR__ . '/../src/system/request.php');
-require_once(__DIR__ . '/../src/system/cookie.php');
-require_once(__DIR__ . '/../src/system/session.php');
-require_once(__DIR__ . '/../src/system/template.php');
-require_once(__DIR__ . '/../src/system/mysql.php');
-require_once(__DIR__ . '/../src/auth.php');
-require_once(__DIR__ . '/../src/user.php');
-require_once(__DIR__ . '/../src/transaction.php');
-require_once(__DIR__ . '/../src/order.php');
-require_once(__DIR__ . '/../src/account.php');
+require_once(__DIR__ . '/../src/loader.php');
+
 
 function run()
 {
---
+++ src/loader.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Stan Gumeniuk i@vigo.su
+ */
+
+require_once(__DIR__ . '/system/config.php');
+require_once(__DIR__ . '/system/request.php');
+require_once(__DIR__ . '/system/cookie.php');
+require_once(__DIR__ . '/system/session.php');
+require_once(__DIR__ . '/system/template.php');
+require_once(__DIR__ . '/system/mysql.php');
+require_once(__DIR__ . '/auth.php');
+require_once(__DIR__ . '/user.php');
+require_once(__DIR__ . '/transaction.php');
+require_once(__DIR__ . '/order.php');
+require_once(__DIR__ . '/account.php');
